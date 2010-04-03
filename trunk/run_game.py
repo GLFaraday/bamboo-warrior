d582bc97642cc0921eea0418785e06a4f846b096
lord.mauve <lord.mauve@1ddce0c1-fa88-5453-4ad4-4d0534897d33>
2010-04-03 22:54:50 +0000

    Fix: improved performance of actor sprites by bypassing Pyglet hooks

--- trunk/run_game.py
+++ trunk/run_game.py
@@ -11,15 +11,22 @@ parser.add_option('-f', '--fullscreen', action='store_true', help='Start fullscr
 parser.add_option('-d', '--resolution', help='Screen or window resolution (WxH)', default='1280x720')
 parser.add_option('-p', '--profiler', action='store_true', help='Run with profiler; print stats on exit', default=False)
 parser.add_option('-r', '--showfps', action='store_true', help='Show framerate display', default=False)
+parser.add_option('-l', '--level', action='store', help='Start a named level')
 
 options, arguments = parser.parse_args()
 
-from bamboo.menu import MenuGameState
 from bamboo.game import Game
 
 game = Game(options)
-state = MenuGameState(game)
-game.set_gamestate(state)
+
+if options.level:
+	from bamboo.gamestate import BambooWarriorGameState
+	state = BambooWarriorGameState(game, [options.level + '.svg'])
+	game.set_gamestate(state)
+else:
+	from bamboo.menu import MenuGameState
+	state = MenuGameState(game)
+	game.set_gamestate(state)
 
 if options.profiler:
 	import cProfile
